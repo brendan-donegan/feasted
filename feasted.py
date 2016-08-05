165cd8c7be98a4c353d9baa5d528390d90fbb3a7
Brendan Donegan <brendan.j.donegan@gmail.com>
2016-08-05 21:25:45 +0100

    Building Feasted backend with Flask

---
+++ feasted.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from flask import (
+    Flask,
+    request,
+)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>Feasted</h1>{}'.format(user_agent)
+
+@app.route('/user/<name>')
+def user(name):
+    return "<h1>{}'s recipes</h1>".format(name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
